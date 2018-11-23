1e7fd0a4983773f7cd86d71d06d78a6fa2e3df26
Taylor Swyter <tswyter@hubspot.com>
2018-11-23 15:02:23 -0500

    adds custom styling to chat

--- js/main.js
+++ js/main.js
@@ -1,30 +1,47 @@
 // add an event listener to the form to submit Dave's message
 const chatForm = document.querySelector('#chatForm');
 const chatInput = chatForm.querySelector('#chatInput');
+const chatArea = document.querySelector('.chat-area');
 const davePane = chatForm.querySelector('#dave');
 const halPane = chatForm.querySelector('#hal');
 
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const input = chatInput.value;
-  davePane.innerHTML += `<p>${input}</p>`;
-  halResponds(input);
+  chatInput.value = '';
+  if (input.length > 0) {
+    chatArea.innerHTML += `<div class="dave"><p>${input}</p></div>`;
+    processResponse(input);
+  }
 });
 
 // create a function for HAL to respond to Dave's messages with variable logic based upon
 // Dave's inputs
-const halResponds = (input) => {
+const processResponse = (input) => {
   const sanitizedInput = input.toLowerCase();
   if (sanitizedInput.indexOf('hi') > -1 || sanitizedInput.indexOf('hello') > -1) {
-    halPane.innerHTML += `<p>How are you today, Dave?</p>`;
-  } else if (sanitizedInput.indexOf('not dave')) {
-    halPane.innerHTML += `<p>Oh, you're not Dave?</p>`;
+    halResponds(`How are you today, Dave?`);
+  } else if (sanitizedInput.indexOf('not dave') > -1) {
+    halResponds(`Oh, you're not Dave? What is your name then?`);
+  } else if (sanitizedInput.indexOf('my name is') > -1) {
+    const name = input.split('is')[1];
+    halResponds(`Oh, nice to meet you, ${name}.`);
+  } else if (sanitizedInput.indexOf('not morning') > -1){
+    halResponds(`Oh, really? What time is it?`);
+  } else {
+    halResponds(`What? You're talking nonsense.`);
   }
+};
+
+const halResponds = (input) => {
+  chatArea.innerHTML +=  `<div class="hal">
+                            <p>${input}</p>
+                          </div>`;
 };
 
 // create a function for HAL to open the chat with "Good morning, Dave"
 function beginConversation() {
-  halPane.innerHTML = `<p>Good Morning, Dave.</p>`
+  halResponds(`Good Morning, Dave.`);
 };
 
 // invoke the opening message 
